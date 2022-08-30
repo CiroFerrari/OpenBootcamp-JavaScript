482ace3dcc86ca7fb470bf5150eb1acedb7a8a45
Franco Ciro Ferrari <francociroferrari@gmail.com>
2022-08-30 18:17:08 -0300

    Ejercicios Tema 2

---
+++ Tema2/Tema2.js
@@ -0,0 +1,12 @@
+let nombre = "Ciro";
+let edad = 30;
+let desarrollador = true;
+let nacimiento = new Date('1991 6 13')
+let libro = {
+  titulo: "Biblia",
+  autor: "Dios",
+  fecha: "0",
+  url: "https://www.bible.com/es/"
+}
+
+let yo = [nombre, edad, desarrollador, nacimiento, libro];
