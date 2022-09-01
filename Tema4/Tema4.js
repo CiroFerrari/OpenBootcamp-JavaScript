1dad094bdee4bb4a21704fc56ac47326217c0607
Franco Ciro Ferrari <francociroferrari@gmail.com>
2022-09-01 13:37:04 -0300

    Ejercicios Tema 4

---
+++ Tema4/Tema4.js
@@ -0,0 +1,19 @@
+let nombre = "Franco Ciro";
+
+let apellido = "Ferrari";
+
+let estudiante = nombre + " " + apellido;
+
+let estudianteMayus = estudiante.toUpperCase();
+
+let estudianteMinus = estudiante.toLowerCase();
+
+let estudianteLongitud = estudiante.length;
+
+let nombreFirst = nombre[0];
+
+let apellidoLast = apellido[apellido.length-1];
+
+let estudianteSinEspacios = estudiante.trim();
+
+let isName = estudiante.includes(nombre);
