68ef80d826e2c0c7801689b6ba85caf2e9174220
Franco Ciro Ferrari <francociroferrari@gmail.com>
2022-09-01 17:46:12 -0300

    Ejercicios Tema 5

---
+++ Tema5/Tema5.js
@@ -0,0 +1,11 @@
+let alturaCM = 178;
+
+let alturaM = 1.78;
+
+let pesoKG = 58.5;
+
+let alturaRounded = Math.round(alturaM);
+
+let pesoDownRounded = Math.floor(pesoKG)
+
+let maximoValorJS = Number.MAX_VALUE + 1 == Number.MAX_VALUE
